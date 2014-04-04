9abfc52231546d9a26ab5b1d1ae25b0b78d195f2
Cameron Hartmann <camhart73@gmail.com>
2014-04-04 10:35:53 -0600

    starting to work through bugs with model classes...  current things to figure out.  1.  How to properly extend User class and have it auto create a user when custom user class is created.  2.  What is the best way to build the primary key for messages?

--- pyweb/chat/models.py
+++ pyweb/chat/models.py
@@ -6,16 +6,17 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class ChatUser(models.Model):
-	user = models.OneToOneField(User)
+
+# class ChatUser(User):
+# 	pass
 
 class Message(models.Model):
 	#make message_id alphanumeric unique identifier... index on it instead of primary key?
 	#collisions possible
-	message_id = models.CharField(max_length=36, primary_key=True, editable=False, unique=True, default='Message:messageIdGenerator')
+	message_id = models.CharField(max_length=36, primary_key=True, editable=False, unique=True, default=Message.messageIdGenerator())
 	text = models.CharField(editable=False, max_length=256)
-	timestamp = models.DateTimeField('date submitted')
-	sender = models.ForeignKey(ChatUser)
+	timestamp = models.DateTimeField(auto_now_add=True, verbose_name='date submitted')
+	sender = models.ForeignKey(User)
 
 	@staticmethod
 	def messageIdGenerator():
@@ -23,5 +24,5 @@ class Message(models.Model):
 
 
 class Conversation(models.Model):
-	participants = models.ManyToManyField(ChatUser)
+	participants = models.ManyToManyField(User)
 	messages = models.ManyToManyField(Message)
