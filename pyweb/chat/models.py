af5c628ebcb7c65475c25f1f35a115e5089b093b
Cameron Hartmann <camhart73@gmail.com>
2014-04-11 16:10:55 -0600

    Started work on test cases in test.py

--- pyweb/chat/models.py
+++ pyweb/chat/models.py
@@ -1,28 +1,50 @@
-#import uuid
+import uuid
 import datetime
 from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import UserManager
+
 
 # Create your models here.
 
 
-# class ChatUser(User):
-# 	pass
+class ChatUser(models.Model):
+ 	#user = models.ForeignKey(User)
+ 	user = models.OneToOneField(User)
+
+ 	@staticmethod
+ 	def createUser(username, email=None, password=None, **kwargs):
+ 		cuser = User.objects.create_user(username=username, email=email, password=password, **kwargs)
+ 		return ChatUser(user=cuser)
+
+	def __str__(self):
+		return self.user.username
+ 			
 
 class Message(models.Model):
 	#make message_id alphanumeric unique identifier... index on it instead of primary key?
 	#collisions possible
-	message_id = models.CharField(max_length=36, primary_key=True, editable=False, unique=True, default=Message.messageIdGenerator())
+	def messageIdGenerator():
+		return uuid.uuid4().hex;
+
+	message_id = models.CharField(primary_key=True, max_length=36, unique=True, editable=False, default=messageIdGenerator())
 	text = models.CharField(editable=False, max_length=256)
 	timestamp = models.DateTimeField(auto_now_add=True, verbose_name='date submitted')
-	sender = models.ForeignKey(User)
+	sender = models.ForeignKey(ChatUser)
 
-	@staticmethod
-	def messageIdGenerator():
-		return uuid.uuid4();
+	def save(self, *args, **kwargs):
+		success = False
+		while success==False:
+			try:
+				super(Message, self).save(*args, **kwargs)
+				success = True
+			except models.db.IntegrityError:
+				success = False
+		super.save(self, args, kwargs)
+
 
 
 class Conversation(models.Model):
-	participants = models.ManyToManyField(User)
+	participants = models.ManyToManyField(ChatUser)
 	messages = models.ManyToManyField(Message)
--- pyweb/chat/tests.py
+++ pyweb/chat/tests.py
@@ -1,3 +1,75 @@
 from django.test import TestCase
+from chat.models import ChatUser, Message, Conversation
+import uuid
+import datetime
+from django.utils import timezone
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.models import UserManager
 
-# Create your tests here.
+# Test cases for ChatUser model
+class ChatUserTests(TestCase):
+
+	# Test ChatUser creation
+	def testCreateUser(self):
+		user = ChatUser.createUser(username="aTestUsername")
+		user.save()
+		# Do some basic tests to make sure it's saved into the database properly
+		self.assertEqual("aTestUsername", user.user.username)
+		self.assertEqual("aTestUsername", ChatUser.objects.get(pk=user.pk).user.username)
+		self.assertEqual(user.pk, ChatUser.objects.get(user=user).pk)
+		self.assertEqual(user.user.pk, ChatUser.objects.get(user=user).user.pk)
+		self.assertEqual(len(ChatUser.objects.all()), 1)
+
+
+# Test cases for Message model
+class MessageTests(TestCase):
+	pass
+
+# Test cases for Conversation model
+class ConversationTests(TestCase):
+
+	# Test Conversation creation
+	def testCreateConversation(self):
+		convo = Conversation()
+		convo.save()
+		self.assertEquals(len(Conversation.objects.all()), 1)
+
+	# Test adding/removing of participants
+	def testAddRemoveParticipants(self):
+		user = ChatUser.createUser(username="Cam")
+		user.save()
+		convo = Conversation()
+		convo.save()
+		convo.participants.add(user)
+		users = []
+		convo.save()
+		for x in range(0, 29):
+			users.append(ChatUser.createUser(username="test" + str(x)))
+			users[x].save()
+		convo.participants.add(*users)
+		convo.save()
+		self.assertEqual(len(convo.participants.all()), len(Conversation.objects.get(pk=convo.pk).participants.all()))
+
+		before = len(convo.participants.all())
+		convo.participants.remove(users[0])
+		convo.save()
+		self.assertEqual(before-1, len(convo.participants.all()))
+		convo.participants.remove(*users[1:])
+		self.assertEqual(1, len(convo.participants.all()))
+		self.assertEqual("Cam", convo.participants.all()[0].user.username)
+
+	def testAddRemoveMessages(self):
+		users = []
+		for x in range(0, 33):
+			users.append(ChatUser.createUser(username="test" + str(x)))
+			users[x].save()
+		msgs = []
+		for x in range(0, 99):
+			msgs.append(Message(sender=ChatUser.objects.get(username="test" + str(x/3)).pk, text="some text " + str(x)))
+			msgs[x].save()
+		convo = Conversation()
+		convo.add(*users)
+		convo.add(*msgs)
+
+
