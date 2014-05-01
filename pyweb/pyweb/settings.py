b30a444fcb47244bf4752eef67907876208f3a96
Rob Oakes <roakes@gurulabs.com>
2014-05-01 15:13:47 -0600

    Added a basic log configuration file to settings

--- pyweb/pyweb/settings.py
+++ pyweb/pyweb/settings.py
@@ -85,3 +85,32 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
 	os.path.join(PROJECT_DIR, 'static'),
 ]
+
+# Logging Configuration
+if DEBUG:
+	LOGGING = {
+		'version' : 1,
+		'disable_existing_loggers' : False,
+		'formatters' : {
+			'simple' : {
+				'format' : '%(asctime)s %(levelname)s %(message)s',
+			}
+		},
+		'handlers' : {
+			'console' : {
+				'level' : 'DEBUG',
+				'class' : 'logging.StreamHandler',
+				'formatter' : 'simple',
+			}
+		},
+		'root' : {
+			'level' : 'INFO',
+			'handlers' : ['console',],
+		},
+		'loggers' : {
+			'django.request' : {
+				'handlers' : False,
+				'level' : 'INFO',
+			},
+		},
+	}
